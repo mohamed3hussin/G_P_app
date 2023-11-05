444a32dab94df010f1ae20188d7411942cae8e8c
mohamed1hussin <99546825+mohamed1hussin@users.noreply.github.com>
2023-11-05 20:57:28 +0200

    on boarding screen done

---
+++ lib/features/onBoarding_screen/onBoarding_model.dart
@@ -0,0 +1,9 @@
+class onBoardingModel
+{
+  final int id;
+  final String image;
+  final String title;
+  final description;
+
+  onBoardingModel(this.id,this.image, this.title, this.description);
+}
